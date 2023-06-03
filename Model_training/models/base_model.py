d65ea74a9de9279ba4122d6c04b2b91e6782a020
Benjamin Bjoern Kroeger <kroegerbe@cip.ifi.lmu.de>
2023-06-03 13:25:42 +0200

    satrted work on basemodel

--- Model_training/models/base_model.py
+++ Model_training/models/base_model.py
@@ -4,11 +4,58 @@ import torch
 from torch import nn
 from torch.utils.data import Dataset
 from Model_training.utils.constants import seq_encoding_enum
+from Model_training.utils.metrics import compute_metrics
+import torch.nn.functional as F
+
 
 class BaseModel(pl.LightningModule):
     seq_encoding = seq_encoding_enum.pp
-    def __init__(self,args,train_set:Dataset=None,val_set:Dataset=None,test_set:Dataset=None):
 
-
+    def __init__(self, args, train_set: Dataset = None, val_set: Dataset = None, test_set: Dataset = None):
         super().__init__()
-       
+        self.save_hyperparameters(args)
+
+        self.val_outputs = []
+
+    def _general_step(self, batch, batch_idx, mode):
+        encoded_seqs = batch[0]
+        solubility_scores = batch[1]
+
+        predicted_solubility_scores = self.forward(encoded_seqs)
+
+        loss_func = nn.BCEWithLogitsLoss()
+        loss = loss_func(predicted_solubility_scores, solubility_scores)
+
+        if mode == 'val':
+            metric_dict = compute_metrics(y_true=solubility_scores, y_pred_prob=F.sigmoid(predicted_solubility_scores))
+            metric_dict['val_loss'] = loss
+        if mode == 'train':
+            return {'train_loss': loss}
+
+    def training_step(self, batch, batch_idx):
+        metric_dict = self._general_step(batch=batch, batch_idx=batch_idx, mode='train')
+        self.log_dict(metric_dict)
+
+        return metric_dict
+
+    def validation_step(self,batch,batch_idx):
+        metric_dict = self._general_step(batch=batch, batch_idx=batch_idx, mode='val')
+        self.val_outputs.append(metric_dict)
+        self.log_dict(metric_dict)
+
+        return metric_dict
+
+    def test_step(self):
+        pass
+
+    def train_dataloader(self):
+        pass
+
+    def val_dataloader(self):
+        pass
+
+    def test_dataloader(self):
+        pass
+
+    def configure_optimizers(self):
+        pass
---
+++ Model_training/utils/metrics.py
@@ -0,0 +1,22 @@
+import torch
+from sklearn.metrics import accuracy_score, f1_score, roc_auc_score, matthews_corrcoef, recall_score, precision_score, balanced_accuracy_score
+
+
+def compute_metrics(y_true, y_pred_prob) -> dict:
+    if isinstance(y_true, torch.Tensor):
+        y_true = y_true.cpu().numpy()
+    if isinstance(y_pred_prob, torch.Tensor):
+        y_pred_prob = y_pred_prob.cpu().numpy()
+
+    y_pred = (y_pred_prob > 0.5).astype(int)
+    metric_dict = {
+        'acc': accuracy_score(y_true=y_true, y_pred=y_pred),
+        'bal_acc': balanced_accuracy_score(y_true=y_true, y_pred=y_pred),
+        'f1': f1_score(y_true=y_true, y_pred=y_pred),
+        'recall': recall_score(y_true=y_true, y_pred=y_pred),
+        'precision': precision_score(y_true=y_true, y_pred=y_pred),
+        'auc': roc_auc_score(y_true=y_true, y_score=y_pred_prob),
+        'mcc': matthews_corrcoef(y_true=y_true, y_pred=y_pred)
+    }
+
+    return metric_dict
