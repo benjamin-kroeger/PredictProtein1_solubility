1e095c85ccd4b6a85336998d9269218f9db0adf2
willruth <willruth@cip.ifi.lmu.de>
2023-06-12 08:34:24 +0200

    added new linear model

--- Model_training/models/lin_models.py
+++ Model_training/models/lin_models.py
@@ -19,6 +19,28 @@ class newModel(BaseModel):
 
     def __init__(self, args: Namespace, train_set: Dataset = None, val_set: Dataset = None, test_set: Dataset = None):
         super().__init__(args=args, train_set=train_set, val_set=val_set, test_set=test_set)
+        self.fc1 = nn.Linear(1024, 2)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(2, 1)
 
-    def forward(self,pp_embedds):
-        pass
+    def forward(self, pp_embedds):
+        out = self.fc1(pp_embedds)
+        out = self.relu(out)
+        out = self.fc2(out)
+        return out
+
+
+class newModel2(BaseModel):
+
+    def __init__(self, args: Namespace, train_set: Dataset = None, val_set: Dataset = None,
+                 test_set: Dataset = None):
+        super().__init__(args=args, train_set=train_set, val_set=val_set, test_set=test_set)
+        self.fc1 = nn.Linear(1024, 2)
+        self.relu = nn.LeakyReLU()
+        self.fc2 = nn.Linear(2, 1)
+
+    def forward(self, pp_embedds):
+        out = self.fc1(pp_embedds)
+        out = self.relu(out)
+        out = self.fc2(out)
+        return out
